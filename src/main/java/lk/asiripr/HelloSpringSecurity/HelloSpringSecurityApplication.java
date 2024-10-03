a01d53ee9a429a08319b8990f710c4a5438fae89
asiripr <asiripramodaya2000@gmail.com>
2024-10-03 06:37:32 +0530

    Spring Security Exercises

---
+++ src/main/java/lk/asiripr/HelloSpringSecurity/HelloSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package lk.asiripr.HelloSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/asiripr/HelloSpringSecurity/HelloSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.asiripr.HelloSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
