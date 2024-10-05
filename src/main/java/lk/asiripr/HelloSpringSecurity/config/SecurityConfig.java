8a9d3feee8ed4c66522dc60db710cba2bd40a77c
asiripr <asiripramodaya2000@gmail.com>
2024-10-05 18:12:11 +0530

    jwt

--- src/main/java/lk/asiripr/HelloSpringSecurity/config/SecurityConfig.java
+++ src/main/java/lk/asiripr/HelloSpringSecurity/config/SecurityConfig.java
@@ -1,22 +1,47 @@
 package lk.asiripr.HelloSpringSecurity.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Value("${jwt.key}")
+	private String jwtKey;
+	
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return new InMemoryUserDetailsManager(
-				User.withUsername("dinuja")
-				.password("{noop}1234")
-				.authorities("READ", "ROLE_USER")
-				.build()
-		);
+				User.withUsername("dinuja").password("{noop}1234").authorities("READ", "ROLE_USER").build());
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(auth->auth.anyRequest().hasAuthority("SCOPE_READ"))
+				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+				.httpBasic(Customizer.withDefaults())
+				.build();
+	}
+	
+	@Bean
+	JwtEncoder jwtEncoder() {
+		return new NimbusJwtEncoder(new ImmutableSecret<>(jwtKey.getBytes()));
 	}
 }
